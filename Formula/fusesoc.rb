d9458268ae202576647eff8373830bc93c5f19a4
Hong Jiarong <me@jrhim.com>
2026-08-13 12:52:53 +0800

    Pin FuseSoC 2.4.6 release tag

--- Formula/fusesoc.rb
+++ Formula/fusesoc.rb
@@ -3,8 +3,8 @@ class Fusesoc < Formula
 
   desc "Package manager and build abstraction tool for HDL code"
   homepage "https://fusesoc.net"
-  url "https://files.pythonhosted.org/packages/80/7d/80bd86ba4d4fb5f387f36ea701335930e541a88df9329ac19559880d2938/fusesoc-2.4.6.tar.gz"
-  sha256 "774e0316d57bd4292bbd7e75c75f5c9742929f6c8e08c99858646fdc0103f17c"
+  url "https://github.com/olofk/fusesoc/archive/refs/tags/2.4.6.tar.gz"
+  sha256 "e0ceb507665ee59202701ad161abee9bd1a8fa172ae09973a11b97d65b6ba83d"
   license "BSD-2-Clause"
 
   depends_on "python@3.14"
